d0c399f433fe6572ae6354778cafd537baf9e2e4
ganzorign <ganzorig.ny@gmail.com>
2025-09-12 19:05:34 +0800

    Evening 12th at work

--- src/Content.js
+++ src/Content.js
@@ -1,33 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+
 
 const Content = () => {
-      const hanldeNameChange = () => {
-    const names = ['Alice', 'Bob', 'Charlie'];  
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-  const handleClick = () => {
-    console.log(`Clicked`);
-    
-  }
-  const handleClick2 = (name) => {
-     console.log(`${name} was Clicked` );
-  }
-    const handleClick3 = (e) => {
-        console.log(e.target)               // logs the <button>
-        console.log(e.target.textContent)   // logs the button text
+
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3"
     }
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked}: item);
+    setItems(listItems);
+  }
+ 
 
 
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-             Hello {hanldeNameChange()}!
-        </p> 
-        <button onClick={handleClick}>Click me</button>
-        <button onClick={() => handleClick2('Dave')}>Click me</button>
-        <button onClick={(e) => handleClick3(e)}>Click me</button>
+       <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked} 
+            />
+            <label>{item.item}  </label>
+            <FaTrashAlt 
+            role="button" 
+            tabIndex="0" 
+            />
+          </li>
+        ))}
+               </ul>
     </main>
   )
 }
